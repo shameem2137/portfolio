38f2108bff067430d517cc9de76f708211f77ef1
Shameem <ansari.shameem80@gmail.com>
2021-01-04 01:32:42 +0530

    New Main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,18 @@
 const navbar = document.querySelector('.nav_bar-button');
 const overlay = document.querySelector('.overlay');
+const body = document.getElementsByTagName('body')[0];
 
-navbar.addEventListener('click', () => {
+navbar.addEventListener('click', (e) => {
+  e.stopPropagation();
   navbar.classList.toggle('open');
   overlay.classList.toggle('open');
+});
+
+overlay.addEventListener('click', (e) => {
+  e.stopPropagation();
+});
+
+document.body.addEventListener('click', (e) => {
+  navbar.classList.remove('open');
+  overlay.classList.remove('open');
 });
