70c66372b8a0a9ff6056a40b84fae5dbdb83549b
Shameem <ansari.shameem80@gmail.com>
2021-01-11 17:26:00 +0530

    Added Services and typed js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,29 @@ navbarbtn.addEventListener('click', () => {
   icon.classList.toggle('fa-times');
   icon.classList.toggle('fa-bars');
 });
+
+$('.services-carousel').owlCarousel({
+  loop: true,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+var typed = new Typed('#typed', {
+  strings: ['Shameem Ansari', 'Web Developer', 'Web Designer', 'Freelancer'],
+  typeSpeed: 100,
+  backSpeed: 50,
+  startDelay: 500,
+  backDelay: 1500,
+  loop: true,
+});
